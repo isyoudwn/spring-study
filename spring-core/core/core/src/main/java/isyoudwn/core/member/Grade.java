4205405563389e43dda069298c121b00031b96cb
임채연 <dlacodus0407@gmail.com>
2024-08-19 15:37:29 +0900

    feat : 회원 도메인 개발

---
+++ spring-core/core/core/src/main/java/isyoudwn/core/member/Grade.java
@@ -0,0 +1,6 @@
+package isyoudwn.core.member;
+
+public enum Grade {
+    BASIC,
+    VIP
+}
---
+++ spring-core/core/core/src/main/java/isyoudwn/core/member/MemberService.java
@@ -0,0 +1,6 @@
+package isyoudwn.core.member;
+
+public interface MemberService {
+    void join(Member member);
+    Member findMember(Long memberId);
+}
