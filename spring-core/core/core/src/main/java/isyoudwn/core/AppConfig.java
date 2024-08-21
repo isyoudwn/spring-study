a73f4d321100e078ed589f6326ca594aca4e134d
임채연 <dlacodus0407@gmail.com>
2024-08-21 12:13:18 +0900

    refactor : AppConfig 리팩토링
    AppConfig 파일을 리팩토링 합니다. 메소드 분리

--- spring-core/core/core/src/main/java/isyoudwn/core/AppConfig.java
+++ spring-core/core/core/src/main/java/isyoudwn/core/AppConfig.java
@@ -1,6 +1,8 @@
 package isyoudwn.core;
 
+import isyoudwn.core.discount.DiscountPolicy;
 import isyoudwn.core.discount.FixDiscountPolicy;
+import isyoudwn.core.member.MemberRepository;
 import isyoudwn.core.member.MemberService;
 import isyoudwn.core.member.MemberServiceImpl;
 import isyoudwn.core.member.MemoryMemberRepository;
@@ -9,11 +11,21 @@ import isyoudwn.core.order.OrderServiceImpl;
 
 // 생성자를 통해 객체 주입
 public class AppConfig {
+
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
+
 }
