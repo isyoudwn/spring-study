843ca1d7330b3b6e1294224df7ad81066e41b54f
임채연 <dlacodus0407@gmail.com>
2024-08-23 17:47:52 +0900

    test : 스프링 빈을 조회합니다
    스프링컨테이너에 등록된 빈을 확인하는 테스트 코드를 작성합니다.

---
+++ spring-core/core/core/src/test/java/isyoudwn/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,49 @@
+package isyoudwn.core.beanfind;
+
+import isyoudwn.core.AppConfig;
+import isyoudwn.core.member.MemberService;
+import isyoudwn.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름없이 타입으로 조회")
+
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByName2() {
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회x")
+    void findBeanByNameX() {
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("xxxx", MemberService.class));
+    }
+}
