f76a04c26a65746514b6b945d93dbd936113b7c4
임채연 <dlacodus0407@gmail.com>
2024-08-26 18:32:16 +0900

    test : 싱글톤 클래스를 만들어서 싱글톤 패턴을 테스트합니다.
    싱글톤 클래스를 만들어서 싱글톤으로 클래스를 구현해보고, 객체 2개를 생성해서 해당 객체가 같은 객체인지 테스트합니다.

--- spring-core/core/core/src/test/java/isyoudwn/core/singleton/SingletonTest.java
+++ spring-core/core/core/src/test/java/isyoudwn/core/singleton/SingletonTest.java
@@ -6,7 +6,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class SinglethonTest {
+public class SingletonTest {
 
     @Test
     @DisplayName("스프링 없는 순수한 DI 컨테이너")
@@ -25,5 +25,14 @@ public class SinglethonTest {
 
         // memberService1 != memberService2
         Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        SingletonService instance1 = SingletonService.getInstance();
+        SingletonService instance2 = SingletonService.getInstance();
+
+        Assertions.assertThat(instance1).isSameAs(instance2);
     }
 }
