0d54c6d851605814a9083b62905346db65dfccda
임채연 <dlacodus0407@gmail.com>
2024-08-31 16:52:17 +0900

    feat : 애플리케이션 엔드포인트에 보안 규칙 부여합니다.
    SecurityFilterChain을 이용하여 엔드포인트에 권한 부여를 구성합니다. 모든 요청에 대해 인증을 요구합니다.

--- spring-security/ssia-ch2-ex1/ssia-ch2-ex1/src/main/java/com/isyoudwn/ssia_ch2_ex1/config/ProjectConfig.java
+++ spring-security/ssia-ch2-ex1/ssia-ch2-ex1/src/main/java/com/isyoudwn/ssia_ch2_ex1/config/ProjectConfig.java
@@ -2,11 +2,14 @@ package com.isyoudwn.ssia_ch2_ex1.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectConfig {
@@ -35,5 +38,14 @@ public class ProjectConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+            http.authorizeRequests(authorizeRequests ->
+                    authorizeRequests.anyRequest().authenticated()).
+                    httpBasic(Customizer.withDefaults());
+            return http.build();
+
     }
 }
