fb89a490f4c3d0356e6727ac35dc11d82e0d7a7e
임채연 <dlacodus0407@gmail.com>
2024-08-31 15:40:57 +0900

    feat : PasswordEncoder를 재정의 합니다.
    UserDetailService 재정의에 따른, PasswordEncoder를 재정의합니다. NoOpPasswordEncoder을 이용해, 암호에 암호화나 해시를 적용하지 않은 객체를 반환합니다.

--- spring-security/ssia-ch2-ex1/ssia-ch2-ex1/src/main/java/com/isyoudwn/ssia_ch2_ex1/config/ProjectConfig.java
+++ spring-security/ssia-ch2-ex1/ssia-ch2-ex1/src/main/java/com/isyoudwn/ssia_ch2_ex1/config/ProjectConfig.java
@@ -4,13 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class ProjectConfig {
 
     /**
-     * TODO :
+     * TODO : 엔드포인트 접근 불가를 해결하는 방법
      * 1. 자격 증명이 있는 사용자를 하나 이상 만든다
      * 2. 사용자를 UserDetailsService에서 관리하도록 추가한다.
      * 3. 주어진 암호를 UserDetailsService가 저장하고 관리하는 암호를 이용해 검증하는
@@ -30,7 +32,8 @@ public class ProjectConfig {
         return userDetailsService;
     }
 
-
-
-
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
