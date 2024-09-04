d49015f03c5b32b85e1cdf04105059c2c4120506
임채연 <dlacodus0407@gmail.com>
2024-09-04 23:45:01 +0900

    test : 새로운 설정정보를 기반으로 테스트를 수행합니다.
    새로운 설정정보가 빈 생성 및 의존관계 주입 기능을 잘 수행하는지 테스트를 수행합니다.

---
+++ spring-core/core/core/src/test/java/isyoudwn/core/scan/AutoAppConfigTest.java
@@ -0,0 +1,21 @@
+package isyoudwn.core.scan;
+
+import isyoudwn.core.AutoAppConfig;
+import isyoudwn.core.member.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class AutoAppConfigTest {
+
+    @Test
+    void basicScan() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+
+        MemberService memberService = ac.getBean(MemberService.class);
+
+        assertThat(memberService).isInstanceOf(MemberService.class);
+
+    }
+}
