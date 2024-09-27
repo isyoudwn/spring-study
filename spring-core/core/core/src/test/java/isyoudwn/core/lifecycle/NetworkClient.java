c40ee780fb12041b74b6c9b6241f12674171d27a
임채연 <dlacodus0407@gmail.com>
2024-09-27 12:23:46 +0900

    test : 빈 등록 초기화, 소멸메소드를 close와 init으로 직접 작성합니다.

--- spring-core/core/core/src/test/java/isyoudwn/core/lifecycle/NetworkClient.java
+++ spring-core/core/core/src/test/java/isyoudwn/core/lifecycle/NetworkClient.java
@@ -1,9 +1,7 @@
 package isyoudwn.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient  {
 
     private String url;
 
@@ -31,15 +29,13 @@ public class NetworkClient implements InitializingBean, DisposableBean {
 
 
     // 의존관계 주입이 끝나면 호출
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    public void init() {
         connect();
         call("초기화 연결 메세지");
     }
 
     // 소멸할 때 호출
-    @Override
-    public void destroy() throws Exception {
+    public void close() {
         disconnect();
     }
 }
