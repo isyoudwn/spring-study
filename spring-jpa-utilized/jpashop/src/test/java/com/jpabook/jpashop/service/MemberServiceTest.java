13bd0b7a02b58e5bf7e8e75ffc5bf6f8eb73cb32
임채연 <dlacodus0407@gmail.com>
2024-10-03 17:09:25 +0900

    test : 회원가입과 중복회원 예외 테스트를 작성합니다.
    회원가입이 성공하는 테스트와, 중복회원이 존재하여 예외를 반환하는 테스트를 작성합니다.

---
+++ spring-jpa-utilized/jpashop/src/test/java/com/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,51 @@
+package com.jpabook.jpashop.service;
+
+import com.jpabook.jpashop.domain.Member;
+import com.jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class) // 스프링과 함께 테스트하기 위함
+@SpringBootTest // 스프링 컨테이너 안에서 테스트 실행함
+@Transactional // test 클래스에서는, roll-back이 된다. -> DB에 저장이 안 된다.
+class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("kim");
+
+        //when
+        Long saveId = memberService.join(member);
+
+         //then
+        assertEquals(member, memberRepository.findOne(saveId));
+    }
+
+    @Test
+    public void 중복_회원_예외() throws Exception {
+        //given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        //when
+        memberService.join(member1);
+        Assertions.assertThrows(IllegalStateException.class, () -> {
+            memberService.join(member2);
+        });
+    }
+}
