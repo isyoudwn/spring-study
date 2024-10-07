e72b78b773aba5b7d8a21454df2b5228d0dc20dd
임채연 <dlacodus0407@gmail.com>
2024-10-07 10:08:03 +0900

    feat : 주문 서비스를 개발합니다.
    주문을 하고, 주문을 취소하는 서비스레이서 단의 메서드를 개발합니다.

--- spring-jpa-utilized/jpashop/src/main/java/com/jpabook/jpashop/domain/OrderItem.java
+++ spring-jpa-utilized/jpashop/src/main/java/com/jpabook/jpashop/domain/OrderItem.java
@@ -2,11 +2,14 @@ package com.jpabook.jpashop.domain;
 
 import com.jpabook.jpashop.domain.item.Item;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
     @Id
     @GeneratedValue
@@ -36,6 +39,7 @@ public class OrderItem {
 
         return orderItem;
     }
+
 
     //==비즈니스 로직==//
     public void cancel() {
