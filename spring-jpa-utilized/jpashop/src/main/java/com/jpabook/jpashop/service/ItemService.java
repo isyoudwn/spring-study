ecd2f592745f741d2b2276fb07b77a9394c49a7f
임채연 <dlacodus0407@gmail.com>
2024-10-08 12:55:39 +0900

    feat : 상품 수정을 변경감지로 개발합니다.

--- spring-jpa-utilized/jpashop/src/main/java/com/jpabook/jpashop/service/ItemService.java
+++ spring-jpa-utilized/jpashop/src/main/java/com/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package com.jpabook.jpashop.service;
 
+import com.jpabook.jpashop.domain.item.Book;
 import com.jpabook.jpashop.domain.item.Item;
 import com.jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,15 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    @Transactional
+    public void updateItem(Long itemId, Book param) {
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setPrice(param.getPrice());
+        findItem.setName(param.getName());
+        findItem.setStockQuantity(param.getStockQuantity());
+        findItem.setPrice(param.getPrice());
     }
 
     public List<Item> findItems() {
