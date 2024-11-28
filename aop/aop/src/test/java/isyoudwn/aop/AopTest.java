b06a0176b84083b9e580732cb7470d3b995d4ec3
임채연 <dlacodus0407@gmail.com>
2024-11-28 15:07:33 +0900

    feat : aop 구현

--- aop/aop/src/test/java/isyoudwn/aop/AopTest.java
+++ aop/aop/src/test/java/isyoudwn/aop/AopTest.java
@@ -3,15 +3,18 @@ package isyoudwn.aop;
 
 import isyoudwn.aop.order.OrderRepository;
 import isyoudwn.aop.order.OrderService;
+import isyoudwn.aop.order.aop.AspectV1;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @Slf4j
 @SpringBootTest
+@Import(AspectV1.class)
 public class AopTest {
 
     @Autowired
@@ -23,7 +26,7 @@ public class AopTest {
     @Test
     void aopInfo() {
         log.info("isAopProxy, orderService={}", AopUtils.isAopProxy(orderService));
-        log.info("isAopProxy, orderService={}", AopUtils.isAopProxy(orderRepository));
+        log.info("isAopProxy, orderRepository={}", AopUtils.isAopProxy(orderRepository));
     }
 
     @Test
