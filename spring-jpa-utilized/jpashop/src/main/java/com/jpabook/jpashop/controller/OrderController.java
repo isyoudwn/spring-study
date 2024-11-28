e3b457501a8e29e0f3117459fd81a09883019079
임채연 <dlacodus0407@gmail.com>
2024-11-28 15:56:40 +0900

    폴더를 정리한다

--- spring-jpa-utilized/jpashop/src/main/java/com/jpabook/jpashop/controller/OrderController.java
+++ spring-jpa-utilized/jpashop/src/main/java/com/jpabook/jpashop/controller/OrderController.java
@@ -1,7 +1,9 @@
 package com.jpabook.jpashop.controller;
 
 import com.jpabook.jpashop.domain.Member;
+import com.jpabook.jpashop.domain.Order;
 import com.jpabook.jpashop.domain.item.Item;
+import com.jpabook.jpashop.repository.OrderSearch;
 import com.jpabook.jpashop.service.ItemService;
 import com.jpabook.jpashop.service.MemberService;
 import com.jpabook.jpashop.service.OrderService;
@@ -9,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -39,5 +42,13 @@ public class OrderController {
                         @RequestParam("count") int count) {
         orderService.order(memberId, itemId, count);
         return "redirect:/orders";
+    }
+
+    @GetMapping("/orders")
+    public String orderList(@ModelAttribute("orderSearch")OrderSearch orderSearch, Model model) {
+        List<Order> orders = orderService.findOrders(orderSearch);
+
+        model.addAttribute("orders", orders);
+        return "order/orderList";
     }
 }
